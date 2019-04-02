8886f0dbec41611a5d7f5bd7bb3f901c0ed4e4e6
Cedric M'PASSI <cempassi@student.42.fr>
2019-04-02 14:00:18 +0200

    minor fix backslash

--- srcs/lexer/quotes_states.c
+++ srcs/lexer/quotes_states.c
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 13:34:28 by cempassi          #+#    #+#             */
-/*   Updated: 2019/04/02 13:53:23 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/04/02 13:59:16 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	quote_machine(t_state *machine)
 {
-	ft_printf("current char input : %c\n", *machine->input);
 	ft_strncat(machine->buffer, machine->input, 1);
 	machine->last_state = machine->state;
 	machine->state = OUT;
 	machine->process = out_machine;
-	machine->input++;
+	if (*machine->input)
+		machine->input++;
 }
