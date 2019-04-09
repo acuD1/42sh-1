f7bad87e5fbb4dca070662f309344542679c2016
Cedric M'PASSI <cempassi@student.42.fr>
2019-04-09 15:57:30 +0200

    Parser files

---
+++ includes/parser.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/09 13:39:31 by cempassi          #+#    #+#             */
+/*   Updated: 2019/04/09 14:00:41 by cempassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+# include "lexer.h"
+
+typedef struct	s_exec
+{
+	int		ac;
+	char	**av;
+	char	**env;
+	int		fdin;
+	int		fdout;
+	int		fderror;
+	gid_t	gid;
+}				t_exec;
+#endif
