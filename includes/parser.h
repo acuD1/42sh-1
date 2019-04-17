de110d3e07308d5b5b946463ee14a9bfc46fc836
Cedric M'PASSI <cempassi@student.42.fr>
2019-04-18 00:13:25 +0200

    Final updates to grammar

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 13:39:31 by cempassi          #+#    #+#             */
-/*   Updated: 2019/04/09 14:00:41 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/04/17 23:26:28 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ typedef struct	s_exec
 	int		fderror;
 	gid_t	gid;
 }				t_exec;
+
 #endif
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,8 +6,17 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 13:39:33 by cempassi          #+#    #+#             */
-/*   Updated: 2019/04/09 14:00:31 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/04/17 23:30:07 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+t_list	*token_concat(t_list *tokens)
+{
+	t_list		*concat_lst;
+
+	concat_lst = NULL;
+
+	return (concat_lst);
+}
