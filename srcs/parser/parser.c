750d752d6809785f9833f804123996a059cf4a7d
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-04-24 11:06:12 +0200

    update parser errors

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -7,6 +7,31 @@ static int	need_subprompt(enum e_type state, enum e_type type)
 	return (FALSE);
 }
 
+static void	print_parser_error(enum e_type type)
+{
+	const static char *signs[14] = {"&&", "OR", ";;", "<<", ">>", "<&", ">&"
+		, "<>", "<<-", ">|", "==", "!="};
+	const static char *script[14] = {CASE, DO, DONE, ELIF, ELSE, ESAC, FI, FOR
+									, IF, IN, THEN, UNTIL, WHILE};
+
+	ft_dprintf(2, "21sh: syntax error near unexpected token ");
+	if (type == E_STRING || type == E_QUOTE
+		|| type == E_DB_QUOTE || type == E_EXP)
+		ft_dprintf(2, "`STRING'\n");
+	else if (type < SINGLE_SIGNS)
+		ft_dprintf(2, "`%c'\n", ALLCHAR[type]);
+	else if (type >= SINGLE_SIGNS && type < SIGNS)
+		ft_dprintf(2, "`%c'\n", signs[type - SINGLE_SIGNS]);
+	else if (type >= SIGNS && type < SIGNS + 13)
+		ft_dprintf(2, "`%c'\n", script[type - SIGNS]);
+	else if (type == E_IO_NUMBER)
+		ft_dprintf(2, "`IO'\n");
+	else if (type == E_ASSIGN)
+		ft_dprintf(2, "`ASSIGN (=)'\n");
+	else
+		ft_dprintf(2, "`END'\n");
+}
+
 static int	manage_error_and_subprompt(enum e_type state, enum e_type type,
 										t_list *lst)
 {
@@ -22,8 +47,7 @@ static int	manage_error_and_subprompt(enum e_type state, enum e_type type,
 		ft_dprintf(2, "21sh: \033[33mNEED TO CALL A SUB_PROMPT\033[0m\n");
 		return (TRUE);
 	}
-	ft_dprintf(2, "21sh: syntax error near unexpected token `%c'\n",
-				type < SINGLE_SIGNS ? ALLCHAR[type] : 'S');
+	print_parser_error(type);
 	return (FALSE);
 }
 
