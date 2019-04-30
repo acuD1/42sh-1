8b6d31fed2dbcc0526d8330dd000dbc8249fbbc9
Frédéric FOISSEY <frederic.foissey@gmail.com>
2019-04-30 17:42:31 +0200

    [FIX] reset value for each prompt for unset

--- srcs/builtin/unset_blt.c
+++ srcs/builtin/unset_blt.c
@@ -6,13 +6,26 @@
 /*   By: ffoissey <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 18:11:50 by ffoissey          #+#    #+#             */
-/*   Updated: 2019/04/30 17:32:17 by ffoissey         ###   ########.fr       */
+/*   Updated: 2019/04/30 17:40:18 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include "interface_functions.h"
 
+static void			reset_prompt(t_registry *shell, char **av)
+{
+	if (ft_strequ(*av, "*") == TRUE)
+		fill_interface_related_internals(shell);
+	else if (ft_strequ(*av, "PS1") == TRUE)
+		get_prompt_ps1(shell);
+	else if (ft_strequ(*av, "PS2") == TRUE)
+		add_internal(shell, INT_PS2, INT_PS2_VALUE);
+	else if (ft_strequ(*av, "PS3") == TRUE)
+		add_internal(shell, INT_PS3, INT_PS3_VALUE);
+	else if (ft_strequ(*av, "PS4") == TRUE)
+		add_internal(shell, INT_PS4, INT_PS4_VALUE);
+}
 int8_t				unset_blt(t_registry *shell, char **av)
 {
 	av++;
@@ -25,7 +38,6 @@ int8_t				unset_blt(t_registry *shell, char **av)
 		free_lst(&shell->intern);
 	else if (shell->env != NULL)
 		free_node(&shell->intern, *av);
-	if (ft_strnequ(*av, "PS", 2) == TRUE || ft_strequ(*av, "*") == TRUE)
-		fill_interface_related_internals(shell);
+	reset_prompt(shell, av);
 	return (SUCCESS);
 }
