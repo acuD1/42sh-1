27610b09b06f69a37751d40123c4a1a704a852dd
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-05-02 02:03:10 +0200

    [FEATURE] ENV can launch process

--- srcs/builtin/env_blt.c
+++ srcs/builtin/env_blt.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "builtin.h"
+#include "lexer.h"
 
 t_option			get_option_env(char *s, t_option option)
 {
@@ -45,7 +46,8 @@ static void				ft_fill_with_new_value(t_registry *cpy_shell,
 		variable->data = ft_strdup(ft_strchr(**arg, '=') + 1);
 		add_env(cpy_shell, variable->name, variable->data);
 		(*arg)++;
-		if (variable && variable->name && ft_strequ(variable->name, "PATH") == TRUE)
+		if (variable && variable->name
+				&& ft_strequ(variable->name, "PATH") == TRUE)
 			hash_blt(cpy_shell, NULL);
 		clear_node((void **)&variable);
 	}
@@ -79,27 +81,49 @@ static t_registry		*copy_registry(t_registry *shell, char ***arg,
 	return (cpy_shell);
 }
 
+static char		*concat_param(char **av)
+{
+	char	*new_input;
+	char	*arg;
+	char	*tmp;
+
+	new_input = NULL;
+	while (*av != NULL)
+	{
+		if (*(av + 1))
+			arg = ft_strjoin(*av, " ");
+		else
+			arg = ft_strdup(*av);
+		tmp = new_input;
+		new_input = ft_strjoin(tmp, arg);
+		ft_strdel(&tmp);
+		ft_strdel(&arg);
+		av++;
+	}
+	return (new_input);
+}
 
 int8_t				env_blt(t_registry *shell, char **av)
 {
 	t_option	option;
 	t_registry	*cpy_shell;
+	char		*new_input;
 
 	av++;
 	option = 0;
-	if (*av != NULL && ft_strequ(*av, "-") == TRUE)
-	{
-		av++;
-		option = I_OPT;
-	}
-	else if (((option |= set_options(&av, get_option_env)) == ERROR_OPT))
+	new_input = NULL;
+	if (((option |= set_options(&av, get_option_env)) == ERROR_OPT))
 		return (FAILURE);
 	cpy_shell = copy_registry(shell, &av, option);
 	if (*av == NULL)
 		print_lst(&cpy_shell->env);
+	if (*av != NULL && (new_input = concat_param(av)) != NULL)
+	{
+		cpy_shell->is_interactive = FALSE;
+		execution_pipeline(cpy_shell, lexer(new_input));
+		ft_strdel(&new_input);
+	}
 	free_lst(&(cpy_shell->env));
-/////////////////////////////// SEND TO EXECUTION ////////////////////
-/////////////////////////////// exec(cpy_shell, av); //////////////////
 	free(cpy_shell);
 	cpy_shell = NULL;
 	return (SUCCESS);
