f9674eff7707e9baa95e23aa588a3d2c52186865
Frederic FOISSEY <ffoissey@student.42.fr>
2019-05-04 15:31:16 +0200

    [UPDATE]2 sh test

--- __test_fuzzer/cmd_generate_files.sh
+++ __test_fuzzer/cmd_generate_files.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
+rm -f test_file/* 2>/dev/null
+rmdir test_file 2>/dev/null
 mkdir test_file 2>/dev/null
 cd test_file
 echo $1 > example.test
 for i in {1000..3000};
 	do for f in example.*;
-       		do zzuf -r 0.2 -s $i < "$f" > "$i-$f";
+       		do zzuf -r 0.02 -s $i < "$f" > "$i-$f";
 	       	done;
        	done
 
