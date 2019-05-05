c1dd919ed5c6cdd6767e392454c04bff6712cc5c
Cedric M'Passi <cedricmpassi@gmail.com>
2019-05-05 18:21:47 +0200

    Fix pipe

--- srcs/parser/application_parser/string_parser.c
+++ srcs/parser/application_parser/string_parser.c
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 21:17:49 by cempassi          #+#    #+#             */
-/*   Updated: 2019/05/05 06:15:18 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/05/05 18:17:29 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ void	flush_string(t_parser *parse)
 		parse->process.av[index] = token->data;
 		free(token);
 	}
-	parse->valid++;
 }
 
 void	special_string_parser(t_parser *parse)
